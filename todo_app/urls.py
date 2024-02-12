2a8f6f310c683daf49b81337a3b8afa69dcbf142
Shailendra Saurav <ssaurav9572@gmail.com>
2024-02-12 12:51:06 +0530

    Update urls.py

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -3,16 +3,13 @@ from django.urls import path
 from .views import todo_list, add_todo, delete_todo, toggle_todo
 
 urlpatterns = [
+    # URL pattern for the todo list view
     path('', todo_list, name='todo_list'),
+    # URL pattern for adding a new todo item
     path('add/', add_todo, name='add_todo'),
+    # URL pattern for deleting a todo item
     path('delete/<int:todo_id>/', delete_todo, name='delete_todo'),
+    # URL pattern for toggling the completion status of a todo item
     path('toggle/<int:todo_id>/', toggle_todo, name='toggle_todo'),
 ]
-# todo_project/urls.py
-from django.contrib import admin
-from django.urls import include, path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('todo/', include('todo_app.urls')),
-]
