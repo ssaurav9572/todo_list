9d6f8b7bf669e3672ba06e7f111cbdd814ea7eba
Shailendra Saurav <ssaurav9572@gmail.com>
2024-02-12 12:50:09 +0530

    Update models.py

--- todo_app/models.py
+++ todo_app/models.py
@@ -3,8 +3,11 @@ from django.db import models
 # Create your models here.
 
 class TodoItem(models.Model):
-    title = models.CharField(max_length=200)
-    completed = models.BooleanField(default=False)
+    # Define a model for TodoItem
+    title = models.CharField(max_length=200)  # Field to store the title of the todo item
+    completed = models.BooleanField(default=False)  # Field to store the completion status of the todo item, defaulting to False
 
     def __str__(self):
-        return self.title
+        # Define a string representation for TodoItem instances
+        return self.title  # Return the title of the todo item when it is converted to a string
+
